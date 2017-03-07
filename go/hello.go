3fc0ed8c4fa4a6bcc1bf389f3d04e15c71b5ad39
Pedrog94 <a01675533@itesm.mx>
2017-03-07 16:33:36 +0100

    tutoriales 1 y 2 de go

---
+++ go/hello.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){ //esto es un comentario de linea.
+
+	/*otro comentario
+	es un hello world!!
+	*/
+	
+	fmt.Println("Hello world")
+	fmt.Println("Peter")
+
+
+}
---
+++ go/vars.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	var x int; //definicion de entero
+	var y float64; //definicion de coma flotante
+	
+	x=200
+	y=32;
+	
+	fmt.Println(x)
+	fmt.Println(y)
+
+
+
+}
